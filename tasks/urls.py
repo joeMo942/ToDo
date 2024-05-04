0d8a2c87a2437f21fa8f3804289e8026bf5089f1
joeMo942 <yousefmohamed942@gmail.com>
2024-05-04 23:51:01 +0300

    final project

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,9 @@ from . import views
 urlpatterns=[
 
     path('addtask/',views.addtask,name="addtask"),
+    path('markascompleted/<int:taskid>/',views.markascompleted,name="markascompleted"),
+    path('markasundone/<int:taskid>/',views.markasundone,name="markasundone"),
+    path('edittask/<int:taskid>/',views.edittask,name="edittask"),
+    path('deletetask/<int:taskid>/',views.deletetask,name="deletetask"),
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,41 @@
 from django import http
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import task
 
 def addtask(request):
     taske=request.POST['task']
     task.objects.create(task=taske)
+    return redirect('home')
+
+
+def markascompleted(request,taskid):
+    taske=get_object_or_404(task,id=taskid)
+    taske.iscompleted=True
+    taske.save()
+    return redirect('home')
+
+
+def markasundone(request,taskid):
+    taske=get_object_or_404(task,id=taskid)
+    taske.iscompleted=False
+    taske.save()
+    return redirect('home')
+
+def edittask(request,taskid):
+    taske=get_object_or_404(task,id=taskid)
+    if request.method=='POST':
+        newtask=request.POST['new_task']
+        taske.task=newtask
+        taske.save()
+        return redirect('home')
+    else:   
+        context={
+           'tc' : taske,
+        }
+        return render(request,'edittask.html',context)
+    
+
+def deletetask(request,taskid):
+    taske=get_object_or_404(task,pk=taskid)
+    taske.delete()
     return redirect('home')
